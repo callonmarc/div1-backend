179c6a8dba2033f886ce4342b4bf842760c4ca6b
callonmarc <mxrcus100@gmail.com>
2026-07-25 01:03:13 -0500

    Update create-checkout-session.js

---
+++ api/create-checkout-session.js
@@ -0,0 +1,39 @@
+const Stripe = require("stripe");
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  try {
+    const { cart } = req.body;
+
+    if (!cart || !Array.isArray(cart) || cart.length === 0) {
+      return res.status(400).json({ error: "Cart is empty" });
+    }
+
+    const line_items = cart.map((item) => ({
+      price: item.priceId,
+      quantity: item.quantity,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      line_items,
+      success_url: "https://div1.online/success.html",
+      cancel_url: "https://div1.online/shop.html",
+    });
+
+    res.status(200).json({
+      url: session.url,
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      error: "Something went wrong.",
+    });
+  }
+};
